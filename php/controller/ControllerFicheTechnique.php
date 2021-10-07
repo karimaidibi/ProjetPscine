fcd3c04e8bf3bcad9e03c4cfe1320b087860e89c
Alexandre Piron <alexandre.piron01@etu.umontpellier.fr>
2021-10-07 21:21:54 +0200

    oublié d'inclure ControllerFicheTechnique.php

---
+++ php/controller/ControllerFicheTechnique.php
@@ -0,0 +1,57 @@
+<?php
+class ControllerFicheTechnique{
+
+	protected static $object='FicheTechnique';
+
+	public static function readAll() {
+        $tab_u = ModelFicheTechnique::selectAll();     //appel au modèle pour gerer la BD  //"redirige" vers la vue
+        $view='list';
+        $pagetitle='Liste des fiches techniques';
+        require_once File::build_path(array("view", "view.php"));
+    }
+
+    public static function read(){
+    	if(!is_null(myGet('NumeroFiche'))){
+	    	$NumeroFiche = myGet('NumeroFiche');
+	    	$u = ModelFicheTechnique::select($NumeroFiche);
+	    	if($u==false){
+        		$view='error';
+        		$pagetitle='Page 404';
+	    		require_once File::build_path(array("view", "view.php"));
+	    	}
+	    	else{
+        		$view='detail';
+        		$pagetitle='FicheTechnique ' . $NumeroFiche;
+	    		require_once File::build_path(array("view", "view.php"));
+	    	}
+    	}
+    	else{
+        	$view='error';
+        	$pagetitle='Page 404';
+	    	require_once File::build_path(array("view", "view.php"));
+    	}
+	}
+
+	public static function delete(){
+		if(!is_null(myGet('NumeroFiche'))){
+        	$view='error';
+        	$pagetitle='Page 404';
+	    	require_once File::build_path(array("view", "view.php"));
+		}
+		else{
+			$NumeroFiche = myGet('NumeroFiche');
+			ModelFicheTechnique::delete($NumeroFiche);
+			$tab_u = ModelFicheTechnique::selectAll();
+	        $view='deleted';
+	        $pagetitle='FicheTechnique supprimée';
+		    require_once File::build_path(array("view", "view.php"));
+		}
+	}
+
+	public static function create($nomFicheTechnique,$DescriptionFicheTechnique){
+        $v1 = new ModelFicheTechnique($nomFicheTechnique,$DescriptionFicheTechnique);
+		$v1->save();
+		return $v1->getNumeroFiche();
+	}
+}
+?>
